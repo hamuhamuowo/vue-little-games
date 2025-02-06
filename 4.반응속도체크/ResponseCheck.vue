a3a97992e1f835e3a3afcaff07ece8701a164972
hamuhamuowo <hemdezvous@gmail.com>
2025-02-06 16:12:58 +0900

    ✨Add webpack watch

---
+++ 4.반응속도체크/ResponseCheck.vue
@@ -0,0 +1,23 @@
+<template>
+	<div>
+		<div id="screen">{{}}</div>
+		<div>
+			<div>평균 시간 : {{}}</div>
+			<button @click="onReset">리셋</button>
+		</div>
+	</div>
+</template>
+<script>
+export default {
+	data() {
+		return {
+			result: [],
+			state: "waiting",
+		};
+	},
+	methods: {
+		onReset() {},
+	},
+};
+</script>
+<style></style>
