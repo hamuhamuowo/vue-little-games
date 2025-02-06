70e90ef3194b9b82ee6584061fdfccbb52a67955
hamuhamuowo <hemdezvous@gmail.com>
2025-02-06 16:27:32 +0900

    💄Set component style

--- 4.반응속도체크/ResponseCheck.vue
+++ 4.반응속도체크/ResponseCheck.vue
@@ -1,6 +1,7 @@
 <template>
 	<div>
-		<div id="screen">{{}}</div>
+		<!--:class 로 축약 가능-->
+		<div id="screen" v-bind:class="state">{{}}</div>
 		<div>
 			<div>평균 시간 : {{}}</div>
 			<button @click="onReset">리셋</button>
@@ -20,4 +21,25 @@ export default {
 	},
 };
 </script>
-<style></style>
+<style scoped>
+/** 해당 컴포넌트에서만 이 스타일을 사용하고 싶다면 scoped 붙여준다 */
+#screen {
+	width: 300px;
+	height: 200px;
+	text-align: center;
+	user-select: none;
+}
+
+#screen.waiting {
+	background-color: aqua;
+}
+
+#screen.ready {
+	background-color: red;
+	color: white;
+}
+
+#screen.now {
+	background-color: greenyellow;
+}
+</style>
--- 4.반응속도체크/webpack.config.js
+++ 4.반응속도체크/webpack.config.js
@@ -19,6 +19,10 @@ module.exports = {
 				test: /\.vue$/,
 				use: "vue-loader",
 			},
+			{
+				test: /\.css$/,
+				use: ["vue-style-loader", "css-loader"],
+			},
 		],
 	},
 	plugins: [new VueLoaderPlugin()],
