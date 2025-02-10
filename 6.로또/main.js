39eec851ed7423b5ae166e4c4b2cff40f6e8e88b
hamuhamuowo <hemdezvous@gmail.com>
2025-02-10 15:42:23 +0900

    🎉 Begin lotto generator

---
+++ 6.로또/main.js
@@ -0,0 +1,4 @@
+import { createApp } from "vue"; // 설치한 뷰를 가져온다
+import LottoGenerator from "./LottoGenerator.vue"; // 웹팩은 자바스크립트만 합쳐주니까 이 파일보고 너 자바스크립트 아니잖아 ...? 상태 -> 에러
+
+createApp(LottoGenerator).mount("#root");
