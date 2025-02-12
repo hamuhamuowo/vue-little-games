53f7cf9e388413ce6bce2546641854f70767f510
hamuhamuowo <hemdezvous@gmail.com>
2025-02-12 10:55:26 +0900

    ✨ Add set comment

--- 7.틱택토/TdComponent.vue
+++ 7.틱택토/TdComponent.vue
@@ -10,10 +10,13 @@ export default {
 	},
 	methods: {
 		onclickTd() {
+			const rootData = this.$root.$data;
 			// 자식 컴포넌트가 부모컴포넌트, 최상위컴포넌트 데이터에 접근할 수 있다
-			console.log(this.$root.$data);
-			console.log(this.$parent.$data);
-			this.$root.$data.turn = this.$root.$data.turn === "O" ? "X" : "O"; // 최상위 컴포넌트의 데이터를 변경하는 로직
+			// console.log(this.$root.$data);
+			// console.log(this.$parent.$data);
+			rootData.tableData[this.rowIndex][this.cellIndex] = rootData.turn; // Vue3에서는 프록시 기반의 반응성 시스템 덕에 화면에 즉시 반영됨
+			// 그러나 Vue2에서는 set을 사용해주어야 한다
+			rootData.turn = rootData.turn === "O" ? "X" : "O"; // 최상위 컴포넌트의 데이터를 변경하는 로직
 		},
 	},
 };
--- 7.틱택토/TikTacTo.vue
+++ 7.틱택토/TikTacTo.vue
@@ -21,7 +21,13 @@ export default {
 			turn: "O",
 		};
 	},
-	methods: {},
+	methods: {
+		onChangeData() {
+			// Vue2
+			// this.tableData[1][0] = 'X'; 인덱스로 접근해서 데이터를 변경하는 게 작동하지 않는다
+			// this.$set(this.tableData[1], 0, 'X'); 이런식으로 접근해서 변경해야 했다 혹은 Vue.set()
+		},
+	},
 	mounted() {},
 	beforeDestroy() {},
 };
